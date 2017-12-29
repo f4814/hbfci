576d40289bf3249608dce9a83c676f0c006fd9cf
Fabian Geiselhart <me@f4814n.de>
2017-12-29 21:37:40 +0100

    Cleaup

--- hbfi/Main.hs
+++ hbfi/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy       as B
+import qualified Data.ByteString.Lazy.Char8 as BC
 import           System.IO
 import           System.Environment
 import           Data.Word
 import           Data.Char
 
 data Machine = Machine B.ByteString Word8 B.ByteString
+
+instance Show Machine where
+    show (Machine p a n) = (show . map ord . BC.unpack . B.take 10 $ p) ++
+        show a ++ (show . map ord . BC.unpack . B.take 10 $ p)
+
+empty :: Machine
+empty = Machine (B.repeat 0) 0 (B.repeat 0)
 
 incPtr :: Machine -> Machine
 incPtr (Machine p a n) = Machine (a `B.cons` p) (B.head n) (B.tail n)
@@ -26,45 +34,46 @@ out (Machine _ a _) = putStrLn . show . chr . fromEnum $ a
 inp :: Machine -> IO Machine
 inp (Machine p _ n) = do
     a <- getLine
-    return $ Machine p (ord . fromEnum $ a) n
+    return $ Machine p (fromIntegral . ord . head $ a) n
 
 parens :: String -> (String, String)
 parens x = (body 0 x, after 0 x)
     where body c (x:xs)
-            | c == 0 && x == ']' = []
+            | c == 0 && x == ']' = ""
             | c /= 0 && x == ']' = x : body (c - 1) xs
-            | c == '['           = x : body (c + 1) xs
-            | otherwise          = x : body x xs
+            | x == '['           = x : body (c + 1) xs
+            | otherwise          = x : body c xs
           after c (x:xs)
             | c == 0 && x == ']' = xs
             | c /= 0 && x == ']' = after (c - 1) xs
-            | c == '['           = after (c + 1) xs
+            | x == '['           = after (c + 1) xs
             | otherwise          = after c xs
 
 loop :: String -> Machine -> IO Machine
 loop s m@(Machine p a n)
-  | s == 0 = m
+  | a == 0 = return m
   | otherwise = do
       n <- brf s m
-      loop s m
+      loop s n
 
 brf :: String -> Machine -> IO Machine
-brf [] _ = []
+brf [] m = return m
 brf (x:xs) m =
     case x of
-        '>' -> brf s (incPtr m)
-        '<' -> brf s (decPtr m)
-        '+' -> brf s (incVal m)
-        '-' -> brf s (devVal m)
+        '>' -> brf xs (incPtr m)
+        '<' -> brf xs (decPtr m)
+        '+' -> brf xs (incVal m)
+        '-' -> brf xs (decVal m)
         '.' -> do
-            out
-            brf s m
+            out m
+            brf xs m
         ',' -> do
             n <- inp m
-            brf s n
+            brf xs n
         '[' -> do
             let p = parens xs
-            brf (snd parens) (loop (fst parens) m)
+            l <- loop (fst p) m
+            brf (snd p) l
         _   -> brf xs m
 
 
@@ -73,6 +82,6 @@ main = do
     args <- getArgs
     h <- openFile (args !! 0) ReadMode
     t <- hGetContents h
-    brf t
+    brf t empty
     return ()
 
